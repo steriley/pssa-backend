6c1f8f8411ac4b6dd61079ecd95583648ba66bd1
Ste Riley <ste@steriley.com>
2023-09-25 18:36:47 +0100

    fix: add body parser

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const createError = require('http-errors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const cors = require('cors');
 
 const apiRouter = require('./routes/api');
 
 const app = express();
 
-app.use(cors());
-app.use(logger('dev'));
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
 
 app.use('/api', apiRouter);
 
